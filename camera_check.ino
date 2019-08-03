3e38521b33dc5e09a5eca1a370e99425df37a701
Mike <mikepumford@gmail.com>
2019-08-03 13:45:48 +0100

    first commit

---
+++ camera_check.ino
@@ -0,0 +1,214 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <TimeLib.h>
+#include <NtpClientLib.h>
+
+// Uncomment below line for running Test version on D1 Uno
+// String relayName = "Test"; int relayPin = D4; const int port = 1081; String NVRstream = "11"; String postIP = "http://192.168.1.81:1081";
+
+// Uncomment below line for running Front Cam version on D1 Mini
+String relayName = "Front Cam"; int relayPin = D1; const int port = 1080; String NVRstream = "01"; String postIP = "http://192.168.1.80:1080";
+
+// Uncomment below line for running Back Cam version on D1 Mini
+// String relayName = "Back Cam"; int relayPin = D1; const int port = 1079; String NVRstream = "11"; String postIP = "http://192.168.1.79:1079";
+
+const char* ssid = "PLUSNET-269F";
+const char* password = "pumfpumf";
+
+ESP8266WebServer server(port);
+String page = "";
+const char* www_username = "admin";
+const char* www_password = "pumfpumf";
+
+const char * NVRhost = "192.168.1.71"; // ip or dns
+const uint16_t NVRport = 554;
+
+int8_t timeZone = 0;
+int8_t minutesTimeZone = 0;
+
+String startTime = "";
+String lastRTSPCheck = "None since server start";
+String lastAutoReboot = "None since server start";
+String lastAppReq = "None since server start";
+String lastGoogleReq = "None since server start";
+String lastWebReq = "None since server start";
+
+unsigned long currentMillis = 0;    // stores the value of millis() in each iteration of loop()
+unsigned long previousRTSPcheck = 0;   // will store last time the RTSP feed was checked
+const int RTSPcheckinterval = 300000;  // RTSP check interval - 5 mins
+
+void setup(void) {
+  Serial.begin(115200);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.print(WiFi.localIP());
+  Serial.print(":");
+  Serial.println(port);
+
+  NTP.begin ("uk.pool.ntp.org", timeZone, true, minutesTimeZone);
+  delay(2000);
+  getNTPtime();
+  startTime = (NTP.getTimeDateString());
+
+  server.on("/", HTTP_POST, handleRequest);
+  server.on("/", []() {
+    if (!server.authenticate(www_username, www_password)) {
+      return server.requestAuthentication();
+    }
+    handleUptest();
+  });
+  
+  server.begin();
+  Serial.println("HTTP server started\n");
+}
+
+void loop(void) {
+  currentMillis = millis();
+  server.handleClient();
+  rtspClient();
+}
+
+void handleRequest() {
+  getNTPtime();
+  String message = "Request received....\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  Serial.println(message);
+  Serial.print("Response : ");
+
+  if (server.arg("request") == "1" ) {
+    digitalWrite(relayPin, HIGH);
+    delay(5000);
+    digitalWrite(relayPin, LOW);
+    Serial.print(relayName);
+    Serial.println(" Rebooted\n");
+    previousRTSPcheck = currentMillis;
+    if (server.arg("source") == "app" ) {
+      lastAppReq = (NTP.getTimeDateString());
+      // server.send(200, "text/plain", "Done!");  // response to http post request
+      server.send(200);
+    }
+    if (server.arg("source") == "google" ) {
+      lastGoogleReq = (NTP.getTimeDateString());
+      // server.send(200, "text/plain", "Done!");  // response to http post request
+      server.send(200);
+    }
+    if (server.arg("source") == "web" ) {
+      lastWebReq = (NTP.getTimeDateString());
+      server.sendHeader("refresh", "0");
+      server.send(200);
+    }
+  }
+}
+
+void handleUptest() {
+  page = "<html>\n";
+  page += "<head>\n";
+  page += "<meta http-equiv='refresh' content='5'/>\n";
+  page += "<title>Wemos D1 " + relayName + "</title>\n";
+  page += "<style>\n";
+  page += "body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\n";
+  page += "</style>\n";
+  page += "</head>\n";
+  page += "<body>\n";
+  page += "<h1><center>Wemos D1 " + relayName + " HTTP Server Running</center></h1>\n";
+  page += "<p><center>Server Start Time: " + startTime + "</center></p>\n";
+  page += "<p><center>Last NVR RTSP Check: " + lastRTSPCheck + "</center></p>\n";
+  page += "<p><center>Last Reboot From Failed RTSP Check: " + lastAutoReboot + "</center></p>\n";
+  page += "<p><center>Last Reboot From App User Request: " + lastAppReq + "</center></p>\n";
+  page += "<p><center>Last Reboot From Google Assistant Request: " + lastGoogleReq + "</center></p>\n";
+  
+
+  page += "<form action=\"" + postIP + "\" method=\"post\">\n";
+  page += "<input type=\"hidden\" id=\"request\" name=\"request\" value=\"1\">\n";
+  page += "<input type=\"hidden\" id=\"source\" name=\"source\" value=\"web\">\n";
+  page += "<p><center>Last Reboot From Web User Request: " + lastWebReq + "   <button>Reboot Now</button></center></p>\n";
+  page += "</form>\n";
+
+  page += "</body>\n";
+  page += "</html>\n";
+
+  server.send(200, "text/html", page);
+
+}
+
+void getNTPtime() {
+  Serial.print (NTP.getTimeDateString ()); Serial.print (" ");
+  Serial.println (NTP.isSummerTime () ? "Summer Time. " : "Winter Time. ");
+}
+
+void rtspClient() {
+  if (currentMillis - previousRTSPcheck >= RTSPcheckinterval) {
+    getNTPtime();
+    lastRTSPCheck = (NTP.getTimeDateString ());
+    Serial.print("Check NVR RTSP feed for ");
+    Serial.println(relayName);
+    Serial.print("connecting to: ");
+    Serial.println(NVRhost);
+
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+
+    if (!client.connect(NVRhost, NVRport)) {
+      Serial.println("connection failed");
+      Serial.println("wait 1 sec...");
+      delay(1000);
+      return;
+    }
+
+    Serial.print("connected to: ");
+    Serial.println(NVRhost);
+    Serial.print("port: ");
+    Serial.println(NVRport);
+
+    // This will send the request to the server
+    Serial.print(String("DESCRIBE ") + "rtsp://" + NVRhost + ":" + NVRport + "/" + NVRstream + " RTSP/1.0\r\n" +
+                 "Cseq: 2\r\n\r\n") ;
+    client.print(String("DESCRIBE ") + "rtsp://" + NVRhost + ":" + NVRport + "/" + NVRstream + " RTSP/1.0\r\n" +
+                 "Cseq: 2\r\n\r\n") ;
+
+    //read back one line from server
+    String line = client.readStringUntil('\r');
+    Serial.println(line);
+    Serial.print("Response : ");
+    if (line != "RTSP/1.0 200 OK") {
+      lastAutoReboot = (NTP.getTimeDateString());
+      Serial.println("REBOOT REQUIRED!");
+      digitalWrite(relayPin, HIGH);
+      delay(5000);
+      digitalWrite(relayPin, LOW);
+      Serial.print(relayName);
+      Serial.println(" Rebooted");
+    }
+    else {
+      Serial.println("All ok!");
+    }
+    Serial.println("closing connection");
+    Serial.println("");
+    client.stop();
+    previousRTSPcheck = currentMillis;
+  }
+}
---
+++ temp_sensor.ino
@@ -0,0 +1,190 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <TimeLib.h>
+
+const char *ssid = "tempmonitor";
+const char *password = "pumfpumf";
+
+ESP8266WebServer server(80);
+String page = "";
+float celcius = 0.0;
+float maxCelcius = 0.0;
+String maxCelciusTime = "00:00";
+String status = "OFF";
+int timerStatus = 0;
+int relay = D7;
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into pin 9 on the Arduino
+#define ONE_WIRE_BUS D9 
+// #define ONE_WIRE_BUS D2 *****************************************
+// Setup a oneWire instance to communicate with any OneWire devices
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+unsigned long currentMillis = 0;    // stores the value of millis() in each iteration of loop()
+unsigned long previousTempCheck = 0;   // will store last time temp was checked
+const int tempCheckInterval = 5000;  // interval
+
+unsigned long initTimerCheck = 0;   // will store last time timer was checked
+int timerCheck = 0;  // interval
+
+void setup(void)
+{
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, LOW);
+  // start serial port
+  Serial.begin(115200);
+  Serial.print("Configuring access point...");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  server.on("/", HTTP_POST, handleRequest);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+
+  Serial.println("Dallas Temperature IC Control Library Used");
+
+  // Start up the library
+  sensors.begin();
+}
+
+void loop(void)
+{
+  server.handleClient();
+
+  currentMillis = millis();
+  if (currentMillis - previousTempCheck >= tempCheckInterval) {
+
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    // Serial.print("Temperature is: ");
+    if (String(sensors.getTempCByIndex(0)) != "-127.00") {
+      celcius = sensors.getTempCByIndex(0);
+    }
+    if (celcius > maxCelcius) {
+      maxCelcius = celcius;
+      if (minute() < 10) {
+        maxCelciusTime = (String(hour()) + ":0" + String(minute()));
+      } else {
+        maxCelciusTime = (String(hour()) + ":" + String(minute()));
+      }
+    }
+
+    if (timerCheck > 0) {
+      timerStatus = timerCheck - currentMillis + initTimerCheck;
+      if (timerStatus <= 0) {
+        timerStatus = 0;
+        timerCheck = 0;
+        digitalWrite(relay, LOW);
+        status = "OFF";
+      }
+    }
+    Serial.println(String(celcius) + "," + String(maxCelcius) + "," + String(maxCelciusTime) + "," + status + "," + String(timerStatus));
+
+    previousTempCheck = currentMillis;
+  }
+}
+
+void handleRoot() {
+  page = "<html>\n";
+  page += "<head>\n";
+  page += "<meta http-equiv='refresh' content='5'/>\n";
+  page += "<title>ESP8266</title>\n";
+  page += "<style>\n";
+  page += "body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\n";
+  page += "</style>\n";
+  page += "</head>\n";
+  page += "<body>\n";
+  page += "<h1><center>ESP8266 HTTP Server Running</center></h1>\n";
+  page += "<h1><center>Current temp " + String(celcius) + "</center></h1>\n";
+  page += "</body>\n";
+  page += "</html>\n";
+
+  server.send(200, "text/html", page);
+
+}
+
+void handleRequest() {
+  String message = "Request received....\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  // Serial.println(message);
+  Serial.println(server.arg("request") + " received");
+  if (server.arg("request").startsWith("initTime")) {
+    String tempString = server.arg("request").substring(8);
+    int commaIndex = tempString.indexOf(',');
+    int secondCommaIndex = tempString.indexOf(',', commaIndex + 1);
+    int thirdCommaIndex = tempString.indexOf(',', secondCommaIndex + 1);
+    int fourthCommaIndex = tempString.indexOf(',', thirdCommaIndex + 1);
+    int fifthCommaIndex = tempString.indexOf(',', fourthCommaIndex + 1);
+    int tempHour = String(tempString.substring(0, commaIndex)).toInt();
+    int tempMins = String(tempString.substring(commaIndex + 1, secondCommaIndex)).toInt();
+    int tempSecs = String(tempString.substring(secondCommaIndex + 1, thirdCommaIndex)).toInt();
+    int tempDay = String(tempString.substring(thirdCommaIndex + 1, fourthCommaIndex)).toInt();
+    int tempMonth = String(tempString.substring(fourthCommaIndex + 1, fifthCommaIndex)).toInt();
+    int tempYear = String(tempString.substring(fifthCommaIndex + 1)).toInt();
+    setTime(tempHour, tempMins, tempSecs, tempDay, tempMonth, tempYear);
+    Serial.print("Time set to " );
+    Serial.print(hour());
+    if (minute() < 10) {
+      Serial.print(":0");
+    } else {
+      Serial.print(":");
+    }
+    Serial.print(minute());
+    if (second() < 10) {
+      Serial.print(":0");
+    } else {
+      Serial.print(":");
+    }
+    Serial.println(second());
+  }
+  if (server.arg("request") == "resetMax" ) {
+    maxCelcius = 0.0;
+  }
+  else if (server.arg("request") == "on" ) {
+    digitalWrite(relay, HIGH);
+    status = "ON";
+  }
+  else if (server.arg("request") == "off" ) {
+    digitalWrite(relay, LOW);
+    status = "OFF";
+    timerStatus = 0;
+    timerCheck = 0;
+  }
+  else if (server.arg("request") == "timernone" ) {
+    timerStatus = 0;
+    timerCheck = 0;
+  }
+  else if (server.arg("request") == "timer2" ) {
+    timerCheck = 120000;
+    initTimerCheck = currentMillis;
+  }
+  else if (server.arg("request") == "timer5" ) {
+    timerCheck = 300000;
+    initTimerCheck = currentMillis;
+  }
+  else if (server.arg("request") == "timer10" ) {
+    timerCheck = 600000;
+    initTimerCheck = currentMillis;
+  }
+  server.send(200, "text/plain", String(celcius) + "," + String(maxCelcius) + "," + String(maxCelciusTime) + "," + status + "," + String(timerStatus));
+}
